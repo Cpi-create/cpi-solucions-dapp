4063221b3037718fe2f66c366748643cbd825984
Leandro Ariel Pasquero <leandropasquero@gmail.com>
2024-11-27 22:04:39 -0600

    Corrigiendo index.html y preparando nueva compilación

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // Nueva API de React 18
+import ReactDOM from "react-dom/client"; // Correcto para React 18
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
