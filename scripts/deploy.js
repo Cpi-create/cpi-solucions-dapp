c65bf9a687057eef1187d58102f82993d1344c26
Leandro Ariel Pasquero <leandropasquero@gmail.com>
2024-12-01 20:49:06 -0600

    Corrección del script de despliegue para aceptar los argumentos correctos

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,21 @@
-// Importar ethers desde Hardhat
 const { ethers } = require("hardhat");
 
 async function main() {
+    const name = "CPIFinancialToken"; // Nombre del token
+    const symbol = "CPI"; // Símbolo del token
+    const admin = "0xc29C9bd0BC978dFCAd34Be1AE66D8E785C152c55"; // Dirección del administrador
+    const usdcToken = "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359"; // Dirección del contrato USDC en Polygon
+
     console.log("Deploying CPIFinancialToken...");
-
-    // Dirección USDC y del propietario inicial
-    const usdcAddress = "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359";
-    const initialOwner = "0xc29C9bd0BC978dFCAd34Be1AE66D8E785C152c55";
-
-    // Obtener la fábrica del contrato y desplegarlo
     const CPIFinancialToken = await ethers.getContractFactory("CPIFinancialToken");
-    const token = await CPIFinancialToken.deploy(usdcAddress, initialOwner);
+    const token = await CPIFinancialToken.deploy(name, symbol, admin, usdcToken);
 
-    console.log("Contract deployed successfully!");
-    console.log("CPIFinancialToken deployed to:", token.target); // token.target en ethers.js 6.x devuelve la dirección del contrato
+    console.log("Awaiting deployment...");
+    await token.deployed();
+
+    console.log("CPIFinancialToken deployed to:", token.address);
 }
 
-// Manejo de errores en el script
 main().catch((error) => {
     console.error("Error al desplegar el contrato:", error);
     process.exitCode = 1;
