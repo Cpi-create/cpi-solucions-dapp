135f1c1872ee8a5fe91f9e46714276ef3b4a13c3
Leandro Ariel Pasquero <leandropasquero@gmail.com>
2024-12-06 19:36:09 -0600

    Corrección completa: Inicialización de holders y estructura del contrato

--- contracts/CPIFinancialToken.sol
+++ contracts/CPIFinancialToken.sol
@@ -39,7 +39,7 @@ contract CPIFinancialToken is ERC20, Ownable, AutomationCompatibleInterface {
         uint256 balance = IERC20(usdcToken).balanceOf(address(this));
         require(balance >= dailyIncome, "No hay suficiente USDC");
 
-        address[] memory holders = getTokenHolders();
+        address[] memory holders = getTokenHolders(); // Obtenemos los titulares
         for (uint256 i = 0; i < holders.length; i++) {
             uint256 reward = (balanceOf(holders[i]) * dailyIncome) / totalSupply();
             IERC20(usdcToken).transfer(holders[i], reward);
@@ -60,11 +60,12 @@ contract CPIFinancialToken is ERC20, Ownable, AutomationCompatibleInterface {
         performData = "";
     }
 
+    // Declaración de titulares del token
     function getTokenHolders() internal view returns (address[] memory) {
-        // Declaramos e inicializamos un array en memoria con longitud 1
+        // Creamos un array en memoria con una longitud fija de 1 (solo el propietario)
         address;
-        holders[0] = owner(); // Asignamos al propietario como único titular
-        return holders; // Devolvemos el array
+        holders[0] = owner(); // Asignamos al propietario como el único titular
+        return holders; // Retornamos el array
     }
 }
 
