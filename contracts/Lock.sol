82cab0be25b8c4107ff42a903e289ce78ab3cafa
Leandro Ariel Pasquero <leandropasquero@gmail.com>
2024-12-07 21:33:50 -0600

    Restauración: Lock.sol restaurado al proyecto

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,29 +1,19 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.27;
-
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17; // Alineado con la versión configurada en hardhat.config.js
 
 contract Lock {
-    uint public unlockTime;
+    uint256 public unlockTime;
     address payable public owner;
 
-    event Withdrawal(uint amount, uint when);
+    event Withdrawal(uint256 amount, uint256 when);
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
+    constructor(uint256 _unlockTime) payable {
+        require(_unlockTime > block.timestamp, "Unlock time should be in the future");
         unlockTime = _unlockTime;
         owner = payable(msg.sender);
     }
 
     function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
         require(block.timestamp >= unlockTime, "You can't withdraw yet");
         require(msg.sender == owner, "You aren't the owner");
 
