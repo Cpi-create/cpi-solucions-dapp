491793a5785d54158e3a1f65d14bedc8e6828607
Leandro Ariel Pasquero <leandropasquero@gmail.com>
2024-12-08 17:43:55 -0600

    Actualización: Valores personalizados en deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,28 @@
 const hre = require("hardhat");
 
 async function main() {
-    const [deployer] = await hre.ethers.getSigners();
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
 
-    console.log("Deploying contracts with the account:", deployer.address);
-    console.log("Account balance:", (await deployer.getBalance()).toString());
+  const accountBalance = await deployer.getBalance();
+  console.log("Account balance:", accountBalance.toString());
 
-    const CPIFinancialToken = await hre.ethers.getContractFactory("CPIFinancialToken");
-    const token = await CPIFinancialToken.deploy(
-        "MyToken",      // Nombre del token
-        "MTK",          // Símbolo del token
-        deployer.address,  // Dirección del administrador
-        "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", // Dirección de USDC en Polygon
-        1000000          // Suministro inicial
-    );
+  // Actualiza los valores según tus necesidades
+  const name = "CPI Financial"; // Nombre del token
+  const symbol = "CPIF"; // Símbolo del token
+  const admin = "0xc29C9bd0BC978dFCAd34Be1AE66D8E785C152c55"; // Dirección del administrador
+  const usdcToken = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"; // Dirección del contrato de USDC en Polygon
+  const initialSupply = hre.ethers.utils.parseUnits("1000000", 18); // Suministro inicial (1 millón de tokens con 18 decimales)
 
-    await token.deployed();
+  const CPIFinancialToken = await hre.ethers.getContractFactory("CPIFinancialToken");
+  const token = await CPIFinancialToken.deploy(name, symbol, admin, usdcToken, initialSupply);
 
-    console.log("CPIFinancialToken deployed to:", token.address);
+  console.log("CPIFinancialToken deployed to:", token.address);
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
