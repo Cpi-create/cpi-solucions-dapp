3779fa6a5a7feec4897b6e6808130af3edf06758
Leandro Ariel Pasquero <leandropasquero@gmail.com>
2024-12-09 00:58:31 -0600

    Corrección: Alineación de valores en deploy.js con configuración verificada

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,11 +5,11 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
 
   const accountBalance = await deployer.getBalance();
-  console.log("Account balance:", accountBalance.toString());
+  console.log("Account balance:", hre.ethers.utils.formatEther(accountBalance), "MATIC");
 
-  // Actualiza los valores según tus necesidades
-  const name = "CPI Financial"; // Nombre del token
-  const symbol = "CPIF"; // Símbolo del token
+  // Valores corregidos
+  const name = "CPIToken"; // Nombre del token
+  const symbol = "CPI"; // Símbolo del token
   const admin = "0xc29C9bd0BC978dFCAd34Be1AE66D8E785C152c55"; // Dirección del administrador
   const usdcToken = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"; // Dirección del contrato de USDC en Polygon
   const initialSupply = hre.ethers.utils.parseUnits("1000000", 18); // Suministro inicial (1 millón de tokens con 18 decimales)
@@ -17,7 +17,7 @@ async function main() {
   const CPIFinancialToken = await hre.ethers.getContractFactory("CPIFinancialToken");
   const token = await CPIFinancialToken.deploy(name, symbol, admin, usdcToken, initialSupply);
 
-  console.log("CPIFinancialToken deployed to:", token.address);
+  console.log("CPIToken deployed to:", token.address);
 }
 
 main()
