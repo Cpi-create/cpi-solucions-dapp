7b28880ae83f7fef66198bdf8152c4fbab434da9
Leandro Ariel Pasquero <leandropasquero@gmail.com>
2024-12-22 15:43:05 -0600

    Código actualizado para desplegar CPIFinancialFactory

--- scripts/deploy_factory.js
+++ scripts/deploy_factory.js
@@ -1,17 +1,20 @@
 const hre = require("hardhat");
 
 async function main() {
+    // Obtenemos el contrato Factory
     const CPIFinancialFactory = await hre.ethers.getContractFactory("CPIFinancialFactory");
+
+    // Desplegamos el contrato
     const factory = await CPIFinancialFactory.deploy();
 
     await factory.deployed();
 
-    console.log("Factory deployed to:", factory.address);
+    console.log("Factory desplegada en la dirección:", factory.address);
 }
 
 main()
     .then(() => process.exit(0))
     .catch((error) => {
-        console.error(error);
+        console.error("Error al desplegar el contrato Factory:", error);
         process.exit(1);
     });
