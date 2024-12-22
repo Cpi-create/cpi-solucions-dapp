87229a32860f31bc8d1f9d6a78e0a0c9915e4177
Leandro Ariel Pasquero <leandropasquero@gmail.com>
2024-12-22 16:24:11 -0600

    Añadidos mensajes de depuración al script deploy_factory.js

--- scripts/deploy_factory.js
+++ scripts/deploy_factory.js
@@ -1,23 +1,21 @@
 const hre = require("hardhat");
 
 async function main() {
-    // Obtenemos el contrato Factory
-    const CPIFinancialFactory = await hre.ethers.getContractFactory("CPIFinancialFactory");
+    console.log("Iniciando el despliegue de CPIFinancialFactory...");
 
-    // Desplegamos el contrato
-    const factory = await CPIFinancialFactory.deploy({
-      gasPrice: hre.ethers.utils.parseUnits('50', 'gwei') // Ajusta el precio del gas si es necesario
-  });
-  
+    const CPIFinancialFactory = await hre.ethers.getContractFactory("CPIFinancialFactory");
+    console.log("Factory obtenida correctamente...");
+
+    const factory = await CPIFinancialFactory.deploy();
+    console.log("Transacción de despliegue enviada...");
 
     await factory.deployed();
-
     console.log("Factory desplegada en la dirección:", factory.address);
 }
 
 main()
     .then(() => process.exit(0))
     .catch((error) => {
-        console.error("Error al desplegar el contrato Factory:", error);
+        console.error("Error al desplegar la factory:", error);
         process.exit(1);
     });
