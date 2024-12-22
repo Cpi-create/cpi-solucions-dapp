78a0eb3e1f2cf53413b716367c3d08d2cfa6ec94
Leandro Ariel Pasquero <leandropasquero@gmail.com>
2024-12-22 16:30:49 -0600

    Ajustes en deploy_factory.js para manejar confirmación de despliegue

--- scripts/deploy_factory.js
+++ scripts/deploy_factory.js
@@ -3,13 +3,20 @@ const hre = require("hardhat");
 async function main() {
     console.log("Iniciando el despliegue de CPIFinancialFactory...");
 
+    // Obtener el contrato
     const CPIFinancialFactory = await hre.ethers.getContractFactory("CPIFinancialFactory");
     console.log("Factory obtenida correctamente...");
 
-    const factory = await CPIFinancialFactory.deploy();
-    console.log("Transacción de despliegue enviada...");
+    // Desplegar el contrato con un límite de gas manual
+    const factory = await CPIFinancialFactory.deploy({
+        gasLimit: 6000000, // Ajustar según sea necesario
+    });
+    console.log("Transacción de despliegue enviada. Esperando confirmación...");
 
+    // Esperar hasta que el contrato sea desplegado
     await factory.deployed();
+
+    // Mostrar la dirección del contrato desplegado
     console.log("Factory desplegada en la dirección:", factory.address);
 }
 
